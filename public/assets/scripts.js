19fd2f35d1a433e341bfe9e1628d5e66d1ada04d
Tim Paul <contact@timpaul.co.uk>
2024-04-22 18:37:32 +0100

    Re-implement form preview and loading spinner

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -1,5 +1,11 @@
 document.getElementById('submit-url').addEventListener('submit', function(e) {
 
-  document.getElementById('submit-url-button').innerText = "Processing ..."
-
+  // Load image preview, loading message and spinner when image is submitted
+  const imageURL = document.getElementById('imageURL').value;
+  document.getElementById('submit-url-button').innerText = "Extracting ..."
+  document.getElementById('formImage').src = imageURL;
+  document.getElementById('formImage').alt = "Original image";
+  document.getElementById('formTabIframe').src = '/loading.html';
+  document.getElementById('listTabIframe').src = '/loading.html';
+  document.getElementById('jsonTabIframe').src = '/loading.html';
 });
